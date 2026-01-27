3426917ab09a8728b2eb9132f32a80ae2a6e39a6
unknown <sungwoo9027@naver.com>
2026-01-27 15:50:35 +0900

    11279 priority queue

---
+++ Q11279.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+
+public class Q11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0;i<N;i++){
+            int a = Integer.parseInt(br.readLine());
+            if(a==0){
+                if(!pq.isEmpty()){
+                    sb.append(pq.poll()+"\n");
+                }else{
+                    sb.append("0\n");
+                }
+            }else{
+                pq.offer(a);
+            }
+        }
+        System.out.println(sb);
+    }
+}
