d41111c6d8e6d01d0476ab02ebee639a10fd4d08
unknown <sungwoo9027@naver.com>
2026-02-11 13:50:23 +0900

    15652

---
+++ Q15652.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q15652 {
+    public static int n,m;
+    public static int[] arr;
+    public static boolean[] visited;
+    public static StringBuilder sb;
+
+    public static void dfs(int start,int a){
+        if(a==m){
+            for(int val : arr){
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start;i<=n;i++){
+            arr[a] = i;
+
+            dfs(i,a+1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visited = new boolean[n+1];
+
+        dfs(1,0);
+
+        System.out.println(sb);
+    }
+}
