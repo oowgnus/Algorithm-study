95202bca848f17c9aee9d673c086d9ba1b485525
unknown <sungwoo9027@naver.com>
2026-02-23 17:18:56 +0900

    1916 다익스트라 한번 더 풀긴했는데 흠 좀 아쉽다 더 해봐야할듯

---
+++ Q1916.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Q1916 {
+    public static int n,m;
+
+    static class Node implements Comparable<Node>{
+        int target;
+        int weight;
+
+        Node(int target, int weight){
+            this.target = target;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o){
+            return this.weight - o.weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        ArrayList<Node>[] adj = new ArrayList[n+1];
+        for(int i=1;i<=n;i++){
+            adj[i] = new ArrayList<>();
+        }
+        
+        for(int i=0;i<m;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            adj[a].add(new Node(b,c));
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int finish = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start,0));
+
+        while (!pq.isEmpty()) {
+            Node curr = pq.poll();
+            int now = curr.target;
+            int d = curr.weight;
+
+            if(dist[now] < d) continue;
+
+            for(Node next : adj[now]){
+                if(dist[next.target] > dist[now] + next.weight){
+                    dist[next.target] = dist[now] + next.weight;
+                    pq.add(new Node(next.target, dist[next.target]));
+                }
+            }
+        }
+
+        System.out.println(dist[finish]);
+    }
+}
