352bc434d67bba34e753f9a39eb6a3f95da4427f
oowgnus <sungwoo9027@naver.com>
2026-03-18 21:12:49 +0900

    2206

--- Q2206.java
+++ Q2206.java
@@ -8,21 +8,26 @@ import java.util.StringTokenizer;
 public class Q2206 {
     public static int[] dx = {0,0,-1,1};
     public static int[] dy = {1,-1,0,0};
-    public static boolean[][] visited;
+    public static boolean[][][] visited;
     public static int n,m;
     public static int[][] index;
-    public static int[][] dist;
+    public static int result;
 
-    public static void bfs(int x, int y){
-        visited[x][y] = true;
+    public static int bfs(int x, int y, int dist, int broken){
+        visited[x][y][broken] = true;
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x,y});
-        boolean flag = false;
+        queue.offer(new int[]{x,y,dist,broken});
 
         while (!queue.isEmpty()) {
             int[] curr = queue.poll();
             int r = curr[0];
             int c = curr[1];
+            int d = curr[2];
+            int b = curr[3];
+            
+            if(r==n && c==m){
+                return d;
+            }
 
             for(int i=0;i<4;i++){
                 int nr = r + dx[i];
@@ -30,18 +35,19 @@ public class Q2206 {
 
                 if(nr<1 || nr>n || nc<1 || nc>m) continue;
 
-                if(!visited[nr][nc] && index[nr][nc]==0){
-                    visited[nr][nc] = true;
-                    dist[nr][nc] = dist[r][c]+1;
-                    queue.offer(new int[]{nr,nc});
-                }else if(!visited[nr][nc] && index[nr][nc]==1 && !flag){
-                    flag = true;
-                    visited[nr][nc] = true;
-                    dist[nr][nc] = dist[r][c]+1;
-                    queue.offer(new int[]{nr,nc});
+                if(!visited[nr][nc][b] && index[nr][nc]==0){
+                    visited[nr][nc][b] = true;
+                    queue.offer(new int[]{nr,nc,d+1,b});
+                }else if(b==0 && index[nr][nc]==1){
+                    if(!visited[nr][nc][1]){
+                        visited[nr][nc][1] = true;
+                        queue.offer(new int[]{nr,nc,d+1,1});
+                    }   
                 }
             }
         }
+        
+        return -1;
     } 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -51,9 +57,8 @@ public class Q2206 {
         m = Integer.parseInt(st.nextToken());
 
         index = new int[n+1][m+1];
-        visited = new boolean[n+1][m+1];
-        dist = new int[n+1][m+1];
-        dist[1][1] = 1;
+        visited = new boolean[n+1][m+1][2];
+
 
         for(int i=1;i<=n;i++){
             String line = br.readLine();
@@ -62,11 +67,8 @@ public class Q2206 {
             }
         }
 
-        bfs(1,1);
-        if(dist[n][m]==0){
-            dist[n][m]=-1;
-        }
-        
-        System.out.println(dist[n][m]);
+        System.out.println(bfs(1,1,1,0));
+
+
     }
 }
