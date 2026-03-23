4237c97a17f971dc4bafd91b8e46267aa3df3dc5
oowgnus <sungwoo9027@naver.com>
2026-03-23 21:11:50 +0900

    1912 dp

---
+++ Q1912.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1912 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] index = new int[n];
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i=0;i<n;i++){
+      index[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int[] dp = new int[n];
+    dp[0] = index[0];
+    for(int i=1;i<n;i++){
+      dp[i] = Math.max(dp[i-1] + index[i], index[i]);
+    }
+
+    int result = Integer.MIN_VALUE;
+    for(int i=0;i<n;i++){
+      result = Math.max(result, dp[i]);
+    }
+
+    System.out.println(result);
+  }
+}
