d29c1034cbba6ce0de42378de24444c62100e2d4
oowgnus <sungwoo9027@naver.com>
2026-03-25 15:39:24 +0900

    2252 DAG

---
+++ Q2252.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Q2252 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    ArrayList<Integer>[] adj = new ArrayList[n+1];
+    for(int i=1;i<=n;i++) adj[i] = new ArrayList<>();
+
+    int[] degree = new int[n+1];
+
+    for(int i=0;i<m;i++){
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+
+      adj[a].add(b);
+      degree[b]++;
+    }
+
+    Queue<Integer> queue = new LinkedList<>();
+    for(int i=1;i<=n;i++){
+      if(degree[i]==0){
+        sb.append(i+" ");
+        queue.offer(i);
+      }
+    }
+
+    while (!queue.isEmpty()) {
+      int curr = queue.poll();
+
+      for(int next : adj[curr]){
+        degree[next]--;
+
+        if(degree[next] == 0){
+          sb.append(next+" ");
+          queue.offer(next);
+        }
+      }
+    }
+
+    System.out.println(sb);
+
+  }
+}
