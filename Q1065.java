7508b3ce7a1cbc5bc331624abea8228b7fa3066a
oowgnus <sungwoo9027@naver.com>
2026-03-31 09:02:57 +0900

    1065 브루트포스

--- Q1065.java
+++ Q1065.java
@@ -7,10 +7,22 @@ public class Q1065 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
 
-    int count = 99;
-
-    for(int i=1;i<=n;i++){
-      
+    int count = 0;
+    if(n>99){
+      count = 99;
+      for(int i=100;i<=n;i++){
+        int a = i/100;
+        int b = (i%100)/10;
+        int c = (i%100)%10;
+        
+        if((a-b)==(b-c)){
+          count++;
+        }
+      }
+    }else{
+      count = n;
     }
+
+    System.out.println(count);
   }
 }
