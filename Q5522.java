ab35837dd26a135b7a538408c9d3166d9c6211d9
oowgnus <sungwoo9027@naver.com>
2026-03-31 09:30:13 +0900

    5522

---
+++ Q5522.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q5522 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int sum=0;
+    for(int i=0;i<5;i++){
+      int a = Integer.parseInt(br.readLine());
+      sum += a;
+    }
+
+    System.out.println(sum);
+  }
+}
